6955cf827349019d231ff0126483bcf9fee6796e
Michael Schneeberger <michael.schneeb@gmail.com>
2025-04-29 16:29:45 +0200

    Update project examples to newest changes.

--- examples/deferexample.py
+++ examples/deferexample.py
@@ -1,25 +1,29 @@
 from donotation import do
 
 import continuationmonad
-from continuationmonad.typing import DeferredObserver, ContinuationCertificate
+from continuationmonad.typing import DeferredHandler, ContinuationCertificate
 
-deferred_observers: list[DeferredObserver] = [None]  # type: ignore
+deferred_handlers: list[DeferredHandler] = [None]  # type: ignore
 certificates: list[ContinuationCertificate] = [None] # type: ignore
 
-def func(_, observer: DeferredObserver):
-    deferred_observers[0] = observer
+def func(_, handler: DeferredHandler):
+    deferred_handlers[0] = handler
     return certificates[0]
 
 @do()
 def defer_and_connect():
     trampoline = yield continuationmonad.get_trampoline()
 
+    def on_error(exception: Exception):
+        raise exception
+
     certificates[0] = continuationmonad.fork(
         source = (
             continuationmonad.from_(None)
-            .flat_map(lambda _: continuationmonad.from_('defer').connect(deferred_observers))
+            .flat_map(lambda _: continuationmonad.from_('defer').connect(deferred_handlers))
             .map(lambda cs: cs[0])
         ),
+        on_error=on_error,
         scheduler=trampoline,
         weight=1,
     )
--- examples/threadingexample.py
+++ examples/threadingexample.py
@@ -4,7 +4,7 @@ import continuationmonad
 s1 = continuationmonad.init_event_loop_scheduler()
 s2 = continuationmonad.init_event_loop_scheduler()
 
-c1 = continuationmonad.schedule_on(s1).map(lambda _: 1)
+c1 = continuationmonad.schedule_with_delay(s1, 0.1).map(lambda _: 1)
 c2 = continuationmonad.schedule_on(s2).map(lambda _: 2)
 
 result = continuationmonad.zip((c1, c2)).run()
